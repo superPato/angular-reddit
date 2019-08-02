ef6eed82ed9d341a298563b95373373830e6e161
Cesar Acual <chechaacual@gmail.com>
2019-08-02 12:22:38 -0600

    Storing Multiple ArticleS

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 import { Article } from "./article.model";
 
 @Component({
@@ -8,15 +8,9 @@ import { Article } from "./article.model";
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  article: Article;
+  @Input() article: Article;
 
-  constructor() { 
-    this.article = new Article(
-      'Angular 2',
-      'http://angular.io',
-      10
-    );
-  }
+  constructor() { }
 
   voteUp() {
     this.article.voteUp();
